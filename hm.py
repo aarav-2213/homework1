ecf40bb8684a46a5498ab9a48cd5ff5161e9ac88
aarav-2213 <naniangels2213@gmail.com>
2025-05-02 17:08:22 +1000

    Add files via upload

---
+++ hm.py
@@ -0,0 +1,13 @@
+# Function to swap three numbers
+
+n1 = 5
+n2 = 15
+n3 = 25
+
+print("Before swapping: n1, n2, n3 are:", n1, n2, n3)
+
+n1, n2, n3 = n3, n1, n2
+
+print("After swapping: n1, n2, n3 are:", n1, n2, n3)
+
+
